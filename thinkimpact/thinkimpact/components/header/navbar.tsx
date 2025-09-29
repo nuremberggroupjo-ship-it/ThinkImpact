f9b6b47296640f03b56586fa5a1a1a6663cbdee9
Nurmbarg <nuremberggroupjo@gmail.com>
2025-09-29 15:53:35 +0300

    omar first commit

--- thinkimpact/thinkimpact/components/header/navbar.tsx
+++ thinkimpact/thinkimpact/components/header/navbar.tsx
@@ -9,30 +9,53 @@ import {
   NavigationMenuList,
   NavigationMenuTrigger,
   NavigationMenuViewport,
+  navigationMenuTriggerStyle,
 } from "@/components/ui/navigation-menu";
 export default function Navbar() {
   return (
     <NavigationMenu viewport={false}>
       <NavigationMenuList>
+                      {/*Home*/}
+                      <NavigationMenuItem>
+          <NavigationMenuLink asChild>
+            <Link className={navigationMenuTriggerStyle()} href="/">Home</Link>
+          </NavigationMenuLink>
+        </NavigationMenuItem>
+        {/*Home*/}
+
+                {/*Consulting*/}
+
         <NavigationMenuItem>
-          <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
+          <NavigationMenuTrigger>Consulting</NavigationMenuTrigger>
 
           <NavigationMenuContent>
             <NavigationMenuLink asChild>
-              <Link href="/docs">Documentation</Link>
+              <Link href="/docs">1</Link>
             </NavigationMenuLink>
           </NavigationMenuContent>
         </NavigationMenuItem>
+        {/*Consulting*/}
+        {/*Training*/}
+
         <NavigationMenuItem>
-          <NavigationMenuTrigger>tow</NavigationMenuTrigger>
+          <NavigationMenuTrigger>Training</NavigationMenuTrigger>
 
           <NavigationMenuContent>
             <NavigationMenuLink asChild>
-              <Link href="/docs">Documentation</Link>
+              <Link href="/docs">1</Link>
             </NavigationMenuLink>
           </NavigationMenuContent>
         </NavigationMenuItem>
+        {/*Training*/}
+                 {/*about*/}
+        <NavigationMenuItem>
+          <NavigationMenuLink asChild>
+            <Link  className={navigationMenuTriggerStyle()} href="/about">About</Link>
+          </NavigationMenuLink>
+        </NavigationMenuItem>
+        {/*about*/}
       </NavigationMenuList>
     </NavigationMenu>
   );
 }
+{/*home consulting training about ourteam*/}
